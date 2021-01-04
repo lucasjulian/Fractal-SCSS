0ab499e3e7e62fb1595062b9187c2bd86344de36
JULIAN Lucas <lucasjulian-dev@outlook.fr>
2021-01-04 22:56:52 +0100

    feat(config): add build command

--- fractal.config.js
+++ fractal.config.js
@@ -23,6 +23,11 @@ fractal.docs.set('path', path.join(__dirname, 'docs'));
 * Tell the Fractal web preview plugin where to look for static assets.
 */
 fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+const builder = fractal.web.builder();
+
+fractal.web.set('builder.dest', __dirname + '/build');
+
 /* change the "assets" tab to the present file type */
 fractal.components.set('resources', {
     scss: {
